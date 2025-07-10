1be0ff438f85ee58be96ce022cb6b4311e9b00d9
moshik <moshik0527669169@gmail.com>
2025-07-10 13:12:49 +0300

    add updeatRiddle

--- meneger.js
+++ meneger.js
@@ -3,19 +3,42 @@ import * as riddleService from './services/riddleService.js'
 async function getAllRiddles(){
     const response = await fetch('http://localhost:2123/riddle/getAll')
     const riddles = await response.json()
-    console.log(riddles)
     return riddles
+}
+
+async function printAllRiddles(){
+    const response = await getAllRiddles()
+    console.log(response)
 }
 
 
 async function addRiddle(){
     const allRiddles = await getAllRiddles()
     const newRiddle = riddleService.creatRiddleObj(allRiddles)
-    fetch('http://localhost:2123/riddle/create', {
+    await fetch('http://localhost:2123/riddle/create', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(newRiddle)
     })
 }
 
-addRiddle()
+
+async function deleteRiddleById(){
+    const id = riddleService.getIdFromeUser()
+    await fetch('http://localhost:2123/riddle/delete', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(id) 
+})
+}
+
+async function updeateRiddle(){
+    const updeat = await riddleService.updeatRid()
+    await fetch('http://localhost:2123/riddle/updeate', {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(updeat)
+})
+}
+
+updeateRiddle()
