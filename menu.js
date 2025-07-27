a9dcdede1e0351d0fb5384a0021769aa6afc398d
moshik <moshik0527669169@gmail.com>
2025-07-27 21:43:38 +0300

    x

--- menu.js
+++ menu.js
@@ -1,44 +1,44 @@
 import promptSync from 'prompt-sync';
 const input = promptSync();
 import { jwtDecode } from 'jwt-decode'
-import {game,addPlayerToDB} from "./game.js";
-import {PlayerObj,login,signup} from './permissions.js'
+import { game, addPlayerToDB } from "./game.js";
+import { PlayerObj, login, signup } from './permissions.js'
 import * as menegerRiddles from './menegerRiddles.js'
 import fs from 'fs/promises'
 let token
 
-async function start(){
-    while(true){
-    console.log('To signup, press 1.')
-    console.log('To login, press 2.')
-    console.log('To enter as a guest, press 3.')
-    let choice = input()
-    switch(choice){
-        case "1":
-            const Player = PlayerObj()
-            await signup(Player)
-            break
-        case "2":
-            const player = PlayerObj()
-            token = await login(player)
-            console.log(token);
-            await fs.writeFile('cookies.txt', token);
-            if(token){
-                const decoded = jwtDecode(token)
-                if (decoded.role == 'user'){
-                    const newGame = await game()
-                    console.log(newGame)
-                    await addPlayerToDB(newGame)
-                }else if(decoded.role == 'admin'){
-                    await menuRiddle()
+async function start() {
+    while (true) {
+        console.log('To signup, press 1.')
+        console.log('To login, press 2.')
+        console.log('To enter as a guest, press 3.')
+        let choice = input()
+        switch (choice) {
+            case "1":
+                const Player = PlayerObj()
+                await signup(Player)
+                break
+            case "2":
+                const player = PlayerObj()
+                token = await login(player)
+                console.log(token);
+                await fs.writeFile('cookies.txt', token);
+                if (token) {
+                    const decoded = jwtDecode(token)
+                    if (decoded.role == 'user') {
+                        const newGame = await game()
+                        console.log(newGame)
+                        await addPlayerToDB(newGame)
+                    } else if (decoded.role == 'admin') {
+                        await menuRiddle()
+                    }
                 }
-            }
-            break
-        case "3":
-            await game()
-            break
-        default:
-            break
+                break
+            case "3":
+                await game()
+                break
+            default:
+                break
         }
     }
 }
